ab488aa5b10d820fcc8e0ff2174ef4dbacf1ece9
sanidhya1404 <79449483+sanidhya1404@users.noreply.github.com>
2021-07-13 16:43:26 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,28 @@
+class Paper{
+    constructor(x, y, r) {
+
+        var options = {
+            isStatic:false,
+            'restitution':0.3,
+            'friction':0.0,
+            'density':1.2
+        }
+        this.x=x
+        this.y=y
+        this.r=r
+        this.image = loadImage("paper.png");
+        this.body = Bodies.circle(this.x, this.y,(this.r-20)/2, options);
+        
+       
+        World.add(world, this.body);
+      }
+      display(){
+        
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        fill("white")
+        imageMode(CENTER);
+        image(this.image, 0, 0, this.width, this.height);
+        pop();
+      }
+}
